ab7bfb6441fa846e01f01b96369e5c061d7acc2b
Hart <pableoh@gmail.com>
2023-01-25 18:22:19 +0800

    first react app finished!

--- my-first-react-app/src/components/Main.js
+++ my-first-react-app/src/components/Main.js
@@ -2,6 +2,17 @@ import { React } from 'react'
 
 export default function Main() {
     return (
-        <div className='container'><h1>This is the main content.</h1></div>
+        <main id='main-content'>
+            <div className='container'>
+                <h1>Fun facts about React</h1>
+                <ul>
+                    <li>Was first released in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 100K stars on GitHub</li>
+                    <li>Is maintained by Facebook</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+            </div>
+        </main>
     )
 }
